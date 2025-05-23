f6f091b8e49e838f996a98b390495dc104374ac9
ganggangme <kga0972@gmail.com>
2025-05-23 12:32:40 +0900

    feat: 카메라 관련 모듈 주석 처리

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from motor.dc.dc_control import DCMotor
 from motor.step.step_control import rotate_step_motor
 from sensor.sr04 import get_distance
-from sensor.camera import detect_gesture
+# from sensor.camera import detect_gesture
 from config import DISTANCE_THRESHOLD
 
 import time
@@ -11,20 +11,20 @@ if __name__ == '__main__':
     dc_motor = DCMotor()
 
     while True:
-        gesture = detect_gesture()
+        # gesture = detect_gesture()
         distance = get_distance()
 
-        if gesture == '1':
-            dc_motor.start()
-        elif gesture == '2':
-            dc_motor.stop()
-        elif gesture == '3':
-            dc_motor.increase_speed()
-        elif gesture == '4':
-            dc_motor.decrease_speed()
+        # if gesture == '1':
+        #     dc_motor.start()
+        # elif gesture == '2':
+        #     dc_motor.stop()
+        # elif gesture == '3':
+        #     dc_motor.increase_speed()
+        # elif gesture == '4':
+        #     dc_motor.decrease_speed()
 
         if distance < DISTANCE_THRESHOLD:
-            dc_motor.increase_speed()
             rotate_step_motor(200)
+            dc_motor.increase_speed()
 
         time.sleep(0.5)
