e88ed4b24dbc6ca390274b9527497d44ff27881c
ganggangme <kga0972@gmail.com>
2025-06-10 09:59:10 +0900

    feat: import 추가

--- sensor/camera.py
+++ sensor/camera.py
@@ -4,6 +4,7 @@ import subprocess
 import mediapipe as mp
 from enum import Enum
 from config import Gesture
+from sensor.camera_stream import start_stream, read_frame
 # Mediapipe 초기화
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
